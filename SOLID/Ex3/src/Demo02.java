907fadaacf7b0a5961df0fe3df1a7b0ece2e877c
Kartik Jangid <kartik.24bcs10259@sst.scaler.com>
2026-02-23 10:04:08 +0530

    Change of file name

--- SOLID/Ex3/src/Demo02.java
+++ SOLID/Ex3/src/Demo02.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
+public class Demo02 {
+    public static void Demo02(String[] args) {
         System.out.println("=== Placement Eligibility ===");
         StudentProfile s = new StudentProfile("23BCS1001", "Ayaan", 8.10, 72, 18, LegacyFlags.NONE);
         EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore());
